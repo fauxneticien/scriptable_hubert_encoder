32748b034c91c80abd517317c70ca8b7d5ff9f4a
Nay San <nay.san@stanford.edu>
2023-09-04 14:34:46 -0700

    Re-factor benchmark script, add backward pass

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,17 @@
 import hydra
 import os
+import warnings
 
-import numpy as np
+import pandas as pd
 
 from omegaconf import DictConfig
 from components._helpers import HuBERTModelForBenchmarking
-from tqdm import tqdm
+
+# Ignore non-relevant warnings
+warnings.filterwarnings("ignore", message="Applied workaround for CuDNN issue")
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 @hydra.main(version_base="1.3", config_path="./config", config_name="default")
 def benchmark(cfg: DictConfig) -> None:
@@ -13,32 +19,16 @@ def benchmark(cfg: DictConfig) -> None:
     for (name, value) in cfg.env.items():
         os.environ[name] = value
 
-    data = hydra.utils.instantiate(cfg.data)
+    model = HuBERTModelForBenchmarking(cfg)
 
-    model = HuBERTModelForBenchmarking(
-        encoder = hydra.utils.instantiate(cfg.encoder),
-        masking_config = cfg.masking
-    )
+    print(f"{cfg.encoder._target_}: {count_parameters(model):,} parameters")
 
-    total_iters = cfg.num_iters + cfg.warmup_iters
+    stats = model.benchmark()
+    stats_df = pd.DataFrame(stats)
 
-    throughput_stats=[]
-
-    for i, batch in (pbar := tqdm(enumerate(data.train_loader), desc="Running warmup", total = total_iters)):
-
-        if i == total_iters:
-            break
-
-        _, throughput = model(batch)
-
-        if i == cfg.warmup_iters:
-            pbar.set_description("Benchmarking throughput")
-
-        if i >= cfg.warmup_iters:
-            throughput_stats.append(throughput)
-
-    # Calculate average without first warm-up step
-    print(f"Throughput (data samples per second): {np.mean(throughput_stats):.2f}")
+    print(f"Throughput (forward+backward): {(stats_df.n_samples / (stats_df.forward + stats_df.backward)).mean():.2f} samples/s")
+    print(f"Forward time in seconds, mean (std): {stats_df.forward.mean():.2f} ({stats_df.forward.std():.2f})")
+    print(f"Backward time in seconds, mean (std): {stats_df.backward.mean():.2f} ({stats_df.backward.std():.2f})")
 
 if __name__ == "__main__":
     benchmark()
--- components/_helpers.py
+++ components/_helpers.py
@@ -1,3 +1,4 @@
+import hydra
 import time
 import torch
 
@@ -7,6 +8,7 @@ from lhotse import CutSet
 from lhotse.dataset import DynamicBucketingSampler
 from lhotse.dataset.collation import collate_custom_field
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 from .verbatim_torchaudio import _get_padding_mask, _compute_mask_indices
 
@@ -43,31 +45,78 @@ class MiniLibriSpeech:
         )
 
 class HuBERTModelForBenchmarking(torch.nn.Module):
-    def __init__(self, encoder, masking_config):
+    def __init__(self, cfg):
         super().__init__()
-        self.encoder = encoder
+        
+        self.cfg = cfg
+
+        self.encoder = hydra.utils.instantiate(cfg.encoder)
+        self.final_proj = torch.nn.Linear(cfg.encoder.embed_dim, cfg.num_pretraining_targets)
+
+        self.loss = hydra.utils.instantiate(cfg.loss)
+
+        self.optimizer = hydra.utils.instantiate(cfg.optim.optimizer, params=self.parameters())
+        self.scheduler = hydra.utils.instantiate(cfg.optim.scheduler, optimizer=self.optimizer)
+
         self.encoder.to('cuda')
+        self.final_proj.to('cuda')
+        self.loss.to('cuda')
 
-        self.masking_config = masking_config
-
-    def forward(self, batch):
-        start=time.time()
+    def forward(self, batch, skip_mask=False, skip_nomask=True):
 
         B, T, C = batch['feats_padded'].shape
 
         padding_mask = _get_padding_mask(batch['feats_padded'], batch['feat_lens'])
-        masks_for_modeling = _compute_mask_indices((B, T), padding_mask, **self.masking_config)
+        masks_for_modeling = _compute_mask_indices((B, T), padding_mask, **self.cfg.masking)
 
         # Zero-out random frames
         batch['feats_padded'][masks_for_modeling] = 0
 
+        self.optimizer.zero_grad()
+
+        start = time.time()
         transformer_outputs = self.encoder(
             batch['feats_padded'].to('cuda'),
             batch['feat_lens'].to('cuda')
         )
 
-        end=time.time()
-        throughput= B/(end-start)
+        mask_m = torch.logical_and(~padding_mask, masks_for_modeling)
+        logits_m = self.final_proj(transformer_outputs[mask_m]) if not skip_mask else None
+        logits_u = self.final_proj(transformer_outputs[~mask_m]) if not skip_nomask else None
+        forward_end=time.time()
 
-        return transformer_outputs, throughput
+        loss = self.loss(logits_m, logits_u, masks_for_modeling, batch["ptlabels_padded"].to('cuda'))
+        loss.backward()
+        backward_end=time.time()
 
+        self.optimizer.step()
+        self.scheduler.step()
+
+        return {
+            'n_samples': B,
+            'forward': forward_end-start,
+            'backward': backward_end-forward_end
+        }
+
+    def benchmark(self):
+
+        data = hydra.utils.instantiate(self.cfg.data)
+
+        total_iters = self.cfg.num_iters + self.cfg.warmup_iters
+
+        throughput_stats=[]
+
+        for i, batch in (pbar := tqdm(enumerate(data.train_loader), desc="Running warmup", total = total_iters, dynamic_ncols=True)):
+
+            if i == total_iters:
+                break
+
+            iter_stats = self.forward(batch)
+
+            if i == self.cfg.warmup_iters:
+                pbar.set_description("Benchmarking throughput")
+
+            if i >= self.cfg.warmup_iters:
+                throughput_stats.append(iter_stats)
+
+        return throughput_stats
